91207fc15a952dacc3da8a331b24f6cad512099f
muraliDomda <murali.domda@gmail.com>
2023-06-05 19:16:10 +0100

    initial commit

--- src/test/java/acceptence/hooks/MyHooks.java
+++ src/test/java/acceptence/hooks/MyHooks.java
@@ -5,6 +5,8 @@ import acceptence.factory.DriverFactory;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.cucumber.java.Scenario;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 
 public class MyHooks {
@@ -27,6 +29,9 @@ public class MyHooks {
     public void after(Scenario scenario) throws InterruptedException {
         System.out.println("AFTER: THREAD ID : " + Thread.currentThread().getId() + "," +
                 "SCENARIO NAME: " + scenario.getName());
+        byte[] screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
+        scenario.attach(screenshot, "image/png", scenario.getName());
+
         try{
             driver.close();
         }
